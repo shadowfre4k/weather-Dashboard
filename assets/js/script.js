18f70a897be60cb60c0709364c3d0992cafd7cd9
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2023-05-09 18:19:02 -0700

    now stores locally stored locally

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,6 @@ var searchBtnEl = document.querySelector(".searchBtn");
 var cityDisplayEl = document.querySelector(".cityDisplay");
 var froecastEl = document.querySelector(".forecast");
 
-//api reminder i just put in the code for reference
-var apiURL =
-  "api.openweathermap.org/data/2.5/forecast? at={lat}&lon={lon}&appid=81e5e2aa364dbd692cb7ce5124ace8ee";
-
 var buttonClickHandler = function (event) {
   //on click handler function will say clickered when the button is clicked to verify it is being clicked
   event.preventDefault();
@@ -19,11 +15,11 @@ var buttonClickHandler = function (event) {
 
   if (cityName) {
     // passes city name that was typed into the function
-    getCityLatLon(cityName);
+    fetchCityLatLon(cityName);
   }
 };
 
-var getCityLatLon = function (city) {
+var fetchCityLatLon = function (city) {
   //function that takes in the input city name
   var geoCodingApi = //api URL to find coordinates of inputted city
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -41,12 +37,12 @@ var getCityLatLon = function (city) {
       var geoLat = data[0].lat; //sets latitude to variable
       var geoLon = data[0].lon; // sets longitude to variable
 
-      getForecast(geoLat, geoLon); //passes longitude and latitude through function to get forecast
+      fetchForecast(geoLat, geoLon); //passes longitude and latitude through function to get forecast
     });
   });
 };
 
-var getForecast = function (lat, lon) {
+var fetchForecast = function (lat, lon) {
   //function to get forecast data
   console.log(lon); //check to see if coordinates pass
   console.log(lat);
@@ -60,17 +56,41 @@ var getForecast = function (lat, lon) {
   fetch(forecastApi).then(function (response) {
     //fetches data from forecast API
     response.json().then(function (data) {
+      var city = data.city.name;
       var cloud = data.list[0].clouds.all; //set path to clouds
       var temp = (data.list[0].main.temp - 273.15) * (9 / 5) + 32; //set path for temp in Fahrenheit
-      var wind = data.list[0].wind.speed; // set path for wind speed
+      var wind = data.list[0].wind.speed * 2.237; // set path for wind speed
       var humidity = data.list[0].main.humidity;
       // console log data to check and make sure all variables are collecting desired stats
       console.log(data);
-      console.log(cloud);
-      console.log(temp);
-      console.log(wind);
-      console.log(humidity);
+      console.log(city);
+      console.log("cloud %: " + cloud);
+      console.log("temperature in F: " + temp);
+      console.log("Wind m/s " + wind);
+      console.log("Humidity %: " + humidity);
+
+      storeCity(city, cloud, temp, wind, humidity);
     });
   });
 };
+
+var storeCity = function (city, cloud, temp, wind, humidty) {
+  var cities = JSON.parse(localStorage.getItem("cities") || "[]");
+  console.log("# of cities: " + cities.length);
+  cities.forEach(function (city, index) {
+    console.log("[" + index + "]: " + city.name);
+  });
+
+  var city = {
+    name: city,
+    cloudCover: cloud,
+    tempF: temp,
+    windSpeed: wind,
+    humidPer: humidty,
+  };
+
+  cities.push(city);
+  console.log(cities);
+};
+
 searchBtnEl.addEventListener("click", buttonClickHandler); //event handler for search button
